1c360c77b4d0f06cf43738035b2ac925fba68c9b
Sushil Rakesh Chaubey <125786068+Sushildevelop@users.noreply.github.com>
2023-08-13 21:43:16 +0530

    Add files via upload

---
+++ Time_complexity.java
@@ -0,0 +1,70 @@
+package Practice;
+
+public class Time_complexity {
+    public static void main(String[] args) {
+         //
+   /*     int a=0;
+        int b=0;
+        for (int i=0;i<n;i++){
+            a=a+i;
+        }
+        for (int j=0;j<M;j++){
+            b=b+j;
+        }
+
+    */
+//        Time complexity = O(N+M) time , O(1) space
+
+
+      /*  int a=0;
+        for (i=0;i<N;i++){
+            for (j=N;j>i;j--){
+                a=a+i+j;
+            }
+        }
+
+       */
+//          Time complexity = O(N*N);
+
+
+
+    /*
+      int i, j,k=0;
+      for (i=n/2;i<=n;i++){
+          for (j=2;j<=n;j=j*2){
+              k=k+n/2;
+          }
+      }
+
+     */
+//        Time Complexity= O(nLogn)
+
+
+   /*
+        int a=0;
+        int i=N;
+        while (i>0){
+            a+=i;
+            i/=2;
+        }
+
+        // Time complexity = O(logN)
+
+    */
+
+     /*
+        for (int i=0;i<n;i++){
+            i*=k
+        }
+
+        // Time complexity = O(logkN)
+        
+      */
+
+
+
+
+
+    }
+
+}
